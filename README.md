9e72f8decc47538677ef83cf2c625d8af94604b5
Johannes Kümmel <jkymmel@gmail.com>
2017-06-27 17:35:09 +0300

    init

---
+++ README.md
@@ -0,0 +1,39 @@
+# liitu.ituk.ee
+
+---
+
+## Requirements
+
+* Node.js
+* Bower
+
+---
+
+## Technologies
+
+* Jade
+* Less
+* Angular.js
+* Gulp
+
+---
+
+## Setup
+
+Run **install.bat** located in root directory of the project.
+
+*Note for Linux users: rename the install.bat to install.sh*
+
+---
+
+## Compiling and Developing
+
+To automagically compile files on edit run:
+
+    gulp
+
+For one-time compile run:
+
+    gulp compile
+
+Compiled files will appear in **dest** directory.
---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+var gulp = require("gulp");
+var jade = require("gulp-jade");
+var less = require("gulp-less");
+var plumber = require("gulp-plumber");
+var sourcemaps = require("gulp-sourcemaps");
+var uglify = require("gulp-uglify");
+var concat = require("gulp-concat");
+var watch = require("gulp-watch");
+var es = require('event-stream');
+//var jslint = require("gulp-jslint");
+//var recess = require("gulp-recess");
+
+var dir = {
+    assets: "assets/",
+    src: "src/",
+    dest: "dest/",
+    bower: "bower_components/"
+};
+
+gulp.task("js", function() {
+    es.concat(
+            gulp.src(dir.src + dir.assets + "js/**/*.js")
+    )
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(concat("app.js"))
+        //.pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(dir.dest + dir.assets + "js/"));
+});
+
+gulp.task("jade", function () {
+    var locations = [
+        {
+            src: "*.jade",
+            dest: ""
+        }
+    ];
+    for(var i = 0; i < locations.length; i++) {
+        gulp.src(dir.src + locations[i].src)
+            .pipe(plumber())
+            .pipe(jade({
+                pretty: true,
+                doctype: "HTML"
+            }))
+            .pipe(gulp.dest(dir.dest + locations[i].dest));
+    }
+});
+
+gulp.task("less", function() {
+    gulp.src(dir.src + dir.assets + "less/master.less")
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(dir.dest + dir.assets + "css/"));
+});
+
+gulp.task("img", function () {
+    gulp.src(dir.src + dir.assets + 'img/**/*.{jpg,png,svg}')
+        .pipe(gulp.dest(dir.dest + dir.assets + 'img/'))
+});
+
+gulp.task("php", function () {
+    gulp.src(dir.src + 'php/*.php')
+        .pipe(gulp.dest(dir.dest + 'php/'))
+});
+
+gulp.task("compile", ["jade", "js", "less", "img", "php"]);
+
+gulp.task("default", ["jade", "js", "less", "img", "php"], function() {
+    gulp.watch(dir.src + dir.assets + "js/**/*.js", function() {
+        gulp.run("js");
+    });
+    gulp.watch(dir.src + dir.assets + "less/**/*.less", function() {
+        gulp.run("less");
+    });
+    gulp.watch(dir.src + "/**/*.jade", function() {
+        gulp.run("jade");
+    });
+    gulp.watch(dir.src + dir.assets + "img/**/*.{jpg,png,svg}", function() {
+        gulp.run("img");
+    });
+    gulp.watch(dir.src + dir.assets + "php/*.php", function() {
+        gulp.run("php");
+    });
+});
