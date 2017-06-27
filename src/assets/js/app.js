331a553517d0bd6f91961950662ea4f0cf0e1589
Johannes Kümmel <jkymmel@gmail.com>
2017-06-27 20:52:05 +0300

    added basic design and vue.js

---
+++ src/assets/js/app.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        joiner: {
+            name: "",
+            personalCode: "",
+            email: "",
+            phone: "",
+            curriculum: ""
+        }
+    }
+});
