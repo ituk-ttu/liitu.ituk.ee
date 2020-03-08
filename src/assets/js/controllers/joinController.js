32aedd010134e93d9a03f1486c754a4f02a3b219
Taavi Ansper <taansp@ttu.ee>
2020-03-08 18:14:28 +0200

    Added functionality for new API implementation.

--- src/assets/js/controllers/joinController.js
+++ src/assets/js/controllers/joinController.js
@@ -3,27 +3,32 @@ app.controller("joinController", ["$q", "$scope", "$stateParams", "$rootScope", 
         $scope.status = "FILLING"; // FILLING, SENDING, ERROR, SUCCESS
 
         $scope.application = {
-            name: "",
+            firstName: "",
+            lastName: "",
             personalCode: "",
             email: "",
-            phone: "",
-            studentCode: ""
+            studentCode: "",
+            curriculum: ""
         };
+
+
 
         $scope.patterns = {
             personalCode: "^[0-9]{11}"
         };
 
+
         $scope.submit = function () {
             if (checkFields()) {
                 $scope.status = "SENDING";
+                $scope.application.curriculum = $scope.application.studentCode.substring(6);
                 $http.post(apiBase, $scope.application)
                     .success(function (application) {
                         $scope.status = "SUCCESS";
                         $state.go('selectMentor', {
                             id: application.id,
                             mentorSelectionCode: application.mentorSelectionCode,
-                            origin: "apply"
+                            origin: "application"
                         });
                     })
                     .error(function (error) {
